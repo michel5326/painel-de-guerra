89c0f81765654c9266e7ccd97279646c059f95bf
DESPACHADOR <DESPACHADOR@PD0516.cmdo.policiamilitar.sp.gov.br>
2026-02-28 03:15:23 -0300

    fix prospect model import

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from db import engine, Base
 
 from core.routes import router as core_router
 from prospecting.routes import router as prospect_router
-from prospecting import models as prospect_models  # IMPORTANTE
+from prospecting import models as prospect_models
 
 app = FastAPI()
 
+# 🔥 DROP apenas prospects (força recriação correta)
+prospect_models.Prospect.__table__.drop(engine, checkfirst=True)
+
+# recria todas
 Base.metadata.create_all(bind=engine)
 
 app.include_router(core_router)
