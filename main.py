0fb7585a42ae454992820217887296e591638be6
DESPACHADOR <DESPACHADOR@PD0516.cmdo.policiamilitar.sp.gov.br>
2026-02-28 01:37:43 -0300

    fix main structure

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from prospecting import models as prospect_models
 from fastapi import FastAPI
 from db import engine, Base
-from core.routes import router
+
+from core.routes import router as core_router
 from prospecting.routes import router as prospect_router
-app.include_router(prospect_router)
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-app.include_router(router)
+app.include_router(core_router)
+app.include_router(prospect_router)
